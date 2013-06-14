820fa809dc53f45f27c4a57626073ac2c8b99677
Kazu Yamamoto <kazu@iij.ad.jp>
2013-06-14 20:17:16 +0900

    working bench.erl.

--- bench.erl
+++ bench.erl
@@ -1,11 +1,8 @@
--module(throughput).
+-module(bench).
 
--export([start_counter/0, start_count/3, counter/0, count/3, nats/1]).
+-export([start_server/0, start_client/2, counter/0, count/2, nats/1]).
 
-nats(0) ->
-  [];
-nats(N) ->
-  [N | nats(N - 1)].
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 counter() ->
   counter(0).
@@ -14,10 +11,20 @@ counter(N) ->
   receive
     {count, List} ->
       counter(N + length(List));
-    {get, Them} ->
-      Them ! N,
+    {get, From} ->
+      From ! N,
       counter(0)
   end.
+
+start_server() ->
+  register(server, spawn(bench, counter, [])).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+nats(0) ->
+  [];
+nats(N) ->
+  [N | nats(N - 1)].
 
 replicate(0, _) ->
   ok;
@@ -25,15 +32,12 @@ replicate(N, What) ->
   What(),
   replicate(N - 1, What).
 
-count(Packets, Size, Them) ->
-  replicate(Packets, fun() -> {counter, Them} ! {count, nats(Size)} end),
-  {counter, Them} ! {get, self()},
+count(Packets, Size) ->
+  replicate(Packets, fun() -> server ! {count, nats(Size)} end),
+  server ! {get, self()},
   receive
-    N -> io:format("Did ~B pings", [N])
+    N -> io:format("Did ~B pings ", [N])
   end.
 
-start_counter() ->
-  register(counter, spawn(throughput, counter, [])).
-
-start_count(Packets, Size, Them) ->
-  count(Packets, Size, Them).
+start_client(Packets, Size) ->
+  count(Packets, Size).
