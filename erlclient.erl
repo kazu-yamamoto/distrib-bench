8dded25fe3f6573ccc677e6d5c733ab68620ab72
Kazu Yamamoto <kazu@iij.ad.jp>
2013-06-18 14:23:29 +0900

    adding stop().

--- erlclient.erl
+++ erlclient.erl
@@ -13,12 +13,13 @@ replicate(N, What) ->
   What(),
   replicate(N - 1, What).
 
-client(Packets, Size, Node) ->
+client(Node, Packets, Size) ->
   replicate(Packets, fun() -> {server, Node} ! {count, nats(Size)} end),
   {server, Node} ! {get, self()},
   receive
     N -> io:format("Did ~B pings ", [N])
   end.
 
-start_client(Packets, Size, Node) ->
-  client(Packets, Size, Node).
+start_client(Node, Packets, Size) ->
+  client(Node, Packets, Size),
+  init:stop().
