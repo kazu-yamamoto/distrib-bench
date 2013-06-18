585331740a9854e19cb59ae235c4d9f6f6e39709
Kazu Yamamoto <kazu@iij.ad.jp>
2013-06-18 14:38:39 +0900

    run noshell.

--- erlclient.erl
+++ erlclient.erl
@@ -1,6 +1,6 @@
 -module(erlclient).
 
--export([start_client/3]).
+-export([start_client/1]).
 
 nats(0) ->
   [];
@@ -20,6 +20,9 @@ client(Node, Packets, Size) ->
     N -> io:format("Did ~B pings ", [N])
   end.
 
-start_client(Node, Packets, Size) ->
+start_client([Arg1,Arg2,Arg3]) ->
+  Node    = list_to_atom(Arg1),
+  Packets = list_to_integer(Arg2),
+  Size    = list_to_integer(Arg3),
   client(Node, Packets, Size),
   init:stop().
